36a9e8fdb309666f8f56d2f3146bb1a393bce9e4
LuGarde1 <lu.garde54@gmail.com>
2024-04-29 11:08:29 +0200

    skk

--- testk.py
+++ testk.py
@@ -1,1 +1,11 @@
-dd
+
+import pandas as pd
+
+# Dateipfad zur Excel-Datei angeben
+excel_datei = "C:\Users\Acer\BettingOddsCalculator\DataFootballData"
+
+# Excel-Datei einlesen
+daten = pd.read_excel(excel_datei)
+
+# Jetzt kannst du mit den Daten arbeiten, z.B. sie anzeigen
+print(daten)
